581219f26e7824ddb16b60e99cb11a8011bf658b
AnthonyRS69 <anthony.richer@isitech.fr>
2026-01-13 10:36:35 +0100

    Prisma

--- components/calculatrice-form.tsx
+++ components/calculatrice-form.tsx
@@ -18,59 +18,54 @@ import {
   FieldSeparator,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
-import { useEffect, useState } from "react"
-
+import { useCalcul } from "../app/hooks/use-calculatrice"
 
 export function CalculatriceForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+const calcul = useCalcul();
+  //   const[nombre1, setNombre1] = useState(0);
+  //   const[nombre2, setNombre2] = useState(0);
+  //   const[resultat, setResult] = useState(0);
 
-    const[nombre1, setNombre1] = useState(0);
-    const[nombre2, setNombre2] = useState(0);
-    const[resultat, setResult] = useState(0);
+  //   const nombre_1 = (e:any) => {
+  //     setNombre1(Number(e.target.value));
+  //   }
+  //   const nombre_2 = (e:any) => {
+  //     setNombre2(Number(e.target.value));
+  //   }   
 
-    const nombre_1 = (e:any) => {
-      setNombre1(Number(e.target.value));
-    }
-    const nombre_2 = (e:any) => {
-      setNombre2(Number(e.target.value));
-    }   
-
-  useEffect(() => {
-    setResult(nombre1 + nombre2);
-    console.log("Resultat: ", resultat);
-  }, [nombre1, nombre2])
+  // useEffect(() => {
+  //   setResult(nombre1 + nombre2);
+  //   console.log("Resultat: ", resultat);
+  // }, [nombre1, nombre2])
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardContent>
+          <div style={{ marginTop: "20px" }}>
+            <strong>Résultat: {calcul.result}</strong>
+          </div>
           <form>
-            <Field>
-                <FieldLabel htmlFor="nombre1">Nombre 1</FieldLabel>
-                    <Input
-                    id="nombre1"
-                    type="number"
-                    value={nombre1}
-                    onChange={nombre_1}
-                    placeholder="Entrez le premier nombre"
-                    />
-            </Field>
-            <Field>
-                <FieldLabel htmlFor="nombre2">Nombre 2</FieldLabel>
-                    <Input
-                    id="nombre2"
-                    type="number"
-                    value={nombre2}
-                    onChange={nombre_2}
-                    placeholder="Entrez le deuxième nombre"
-                    />
-            </Field>
-            <Field>
-                <Button type="button">Calculer</Button>
-            </Field>
-            <div>Résultat: {resultat}</div>
+            <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap: "10px", marginTop: "10px" }}>
+              <Button id="button7" type="button" onClick={() => calcul.handleNumberClick(7)}>7</Button>
+              <Button id="button8" type="button" onClick={() => calcul.handleNumberClick(8)}>8</Button>
+              <Button id="button9" type="button" onClick={() => calcul.handleNumberClick(9)}>9</Button>
+              <Button id="buttonC" type="button" style={{backgroundColor: "red" }} onClick={calcul.clear}>C</Button>
+              <Button id="button4" type="button" onClick={() => calcul.handleNumberClick(4)}>4</Button>
+              <Button id="button5" type="button" onClick={() => calcul.handleNumberClick(5)}>5</Button>
+              <Button id="button6" type="button" onClick={() => calcul.handleNumberClick(6)}>6</Button>
+              <Button id="buttondiv" type="button" onClick={calcul.button3Click}>/</Button>
+              <Button id="button1" type="button" onClick={() => calcul.handleNumberClick(1)}>1</Button>
+              <Button id="button2" type="button" onClick={() => calcul.handleNumberClick(2)}>2</Button>
+              <Button id="button3" type="button" onClick={() => calcul.handleNumberClick(3)}>3</Button>
+              <Button id="buttonfois" type="button" onClick={calcul.button4Click}>*</Button>
+              <Button id="button0" type="button" style={{gridColumn: "span 2" }} onClick={() => calcul.handleNumberClick(0)}>0</Button>
+              <Button id="buttonegal" type="button" onClick={calcul.handleEquals}>=</Button>
+              <Button id="buttonplus" type="button" onClick={calcul.button1Click}>+</Button>
+            </div>
           </form>
         </CardContent>
       </Card>
---
+++ middleware/register.ts
@@ -0,0 +1,24 @@
+import { IRegister } from "@/app/interfaces/users";
+
+export const MRegister = (user : IRegister) => {
+    const { firstName, lastName, email, password, confirmPassword } = user;
+    const a = []
+
+
+    if (firstName.length >= 2 && firstName.length <= 20) {
+        a.push({ error :true, message: "First name is not correct", code: "E01"})
+    }
+    if (lastName.length >= 2 && lastName.length <= 2) {
+        a.push({ error :true, message: "Last name is not correct", code: "E02"})
+    }
+    if (email.length >= 2 && email.length <= 20) {
+        a.push({ error :true, message: "Email is not correct", code: "E03"})
+    }
+    if (password.length >= 2 && password.length <= 20) {
+        a.push({ error :true, message: "Password is not correct", code: "E04"})
+    }
+    if (password !== confirmPassword) {
+        a.push({ error :true, message: "Password is not correct", code: "E05"})
+    }
+ return a;
+}
