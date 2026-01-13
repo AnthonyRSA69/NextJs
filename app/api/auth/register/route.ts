b3bc9056b689e08ced027c61c1e2311f422511a2
AnthonyRS69 <anthony.richer@isitech.fr>
2026-01-13 11:39:52 +0100

    Add Argon2

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,6 +1,8 @@
 import type { NextRequest } from "next/server";
 import type { IRegister } from "@/app/interfaces/users";
 import { MRegister } from "@/middleware/register";
+import { PrismaClient } from "@/lib/generated/prisma/client";
+import { prisma } from "@/lib/prisma";
 export async function POST(request: NextRequest) {
 
     const {firstName, lastName, email, password, confirmPassword} : IRegister = await request.json();
@@ -8,6 +10,20 @@ export async function POST(request: NextRequest) {
 
     if (middle.length >= 0) {
         return Response.json(middle);
+    }
+
+    try {
+        await prisma.user.create({
+            data: {
+                firstName,
+                lastName,
+                email,
+                password,
+            },
+        });
+    } catch (e) {
+        console.log(e);
+        return Response.json({ error: "Register route is working!" });
     }
     return Response.json({ message: "Register route is working!" });
 
